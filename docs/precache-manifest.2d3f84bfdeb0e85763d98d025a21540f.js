5f0fbec6c8eaf621a02938bbeb2b21781a2c50b9
Javier sanchez <sanchez.arisan@gmail.com>
2020-08-03 09:42:43 -0600

    DOcs agregado

---
+++ docs/precache-manifest.2d3f84bfdeb0e85763d98d025a21540f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d5ce8f77add653e3cbaff012398771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4009aa8805297ae6ce11",
+    "url": "/static/css/main.e5546372.chunk.css"
+  },
+  {
+    "revision": "5aa2d828a2c2c3164dd0",
+    "url": "/static/js/2.1155ce77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1155ce77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4009aa8805297ae6ce11",
+    "url": "/static/js/main.038ff9a8.chunk.js"
+  },
+  {
+    "revision": "2a5e0e129a95303d4d56",
+    "url": "/static/js/runtime-main.5f1fa5e1.js"
+  }
+]);
