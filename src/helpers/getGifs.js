c9487e97ef7af099aae75dafb7b9054ceb806945
Javier sanchez <sanchez.arisan@gmail.com>
2020-08-03 09:58:41 -0600

    add https

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export  const getGifs = async (category) => {
-    const url =     `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=N4yVJdpQ4mDAg4XY9ZGC0YfIL3rKZCPo`;
+    const url =     `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=N4yVJdpQ4mDAg4XY9ZGC0YfIL3rKZCPo`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
